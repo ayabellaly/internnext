14f4dd77227c0a1cb3095ae743b5c33a9d6614a7
ayabellaly <ayabellaly2000@gmail.com>
2023-04-30 12:37:21 +0100

    sec commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,13 +20,14 @@ function Home() {
 
             </div>
             <div className="card-body">
-            <h1 className="text-success title text-center "> AL AKHDAR BANK</h1>
+            <h1 className="text-success title text-center "> AL Akhdar BANK</h1>
               <p className="font-italic text-justify h1-container">
               AL AKHDAR BANK (AAB) est une nouvelle banque participative créée conjointement par le Groupe Crédit Agricole du Maroc et la Société Islamique pour le Développement du Secteur Privé (SID), une institution financière multilatérale de développement, filiale du Groupe de la Banque Islamique de Développement. AL AKHDAR BANK s’inscrit dans le cadre de la stratégie du Groupe Crédit Agricole du Maroc visant la diversification des produits proposés à la clientèle. Elle permet, par ailleurs, au Groupe CAM de se distinguer par des solutions bancaires novatrices notamment pour le financement et le développement du secteur agricole et du monde rural.
 
-
+            
 
               </p>
+                <p className="font-italic text-justify h1-container">banque participative créée conjointement par le Groupe Crédit Agricole</p>
               <div className="text-center">              
               <img src="https://varadinum.ma/wp-content/uploads/2022/08/AF1QipMAC_prM8xdYjwbyi4wvPZ6IJOj7VMkSrhe1kZ1w1600-h1000-k-no.jpeg" class=" h-75 w-50" alt="image"/>
 </div>
